608683ae697145b14953cbec7c76ffb4030ac903
John Hession <johhess@cdw.com>
2022-01-28 10:38:24 -0800

    Updating tests

--- cmd/cmd_test.go
+++ cmd/cmd_test.go
@@ -9,7 +9,17 @@ func TestTerraformCmd(t *testing.T) {
 		t.Fail()
 		t.Logf(err.Error())
 	} else {
-		t.Logf("Test passed for TestTerraformCmd!")
+		t.Logf("Test passed for terraform sub command!")
 	}
 }
 
+func TestCodeCmd(t *testing.T) {
+	cmd := NewCodeCommand()
+	err := cmd.Execute()
+	if err != nil {
+		t.Fail()
+		t.Logf(err.Error())
+	} else {
+		t.Logf("Test passed for code sub command!")
+	}
+}
