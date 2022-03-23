dcdea2e93b1c504cc81a56e79c05dd97c5badaa1
John Hession <johhess@cdw.com>
2022-03-22 17:35:10 -0700

    Removing choco stuff

--- paradocs-choco/tools/tfchocolateyinstall.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = 'Stop'
-
-$packageName = 'terraform-docs'
-$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$checksum = 'b1e63cede84ae139045d5385c389232fbbcb369a99496b94ec1cd733b8d4d125'
-$url = 'https://github.com/terraform-docs/terraform-docs/releases/download/v0.16.0/terraform-docs-v0.16.0-windows-amd64.zip'
-$destination = "$toolsPath\terraform-docs.exe"
-
-Install-ChocolateyZipPackage -PackageName $packageName -Url64bit $url -Checksum64 $checksum -ChecksumType64 sha256 -UnzipLocation $destination
