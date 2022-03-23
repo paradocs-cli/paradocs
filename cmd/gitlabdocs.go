9c630a454bcdea5b3868fb9c001b3b4a8cfbfb4d
John Hession <johhess@cdw.com>
2022-03-23 13:02:54 -0700

    Changing command structure for platform documentation

--- cmd/gitlabdocs.go
+++ cmd/gitlabdocs.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2022 John Hession
-
 */
 package cmd
 
@@ -16,15 +15,15 @@ var (
 	builder docbuilder.GitlabData
 )
 
-// gitlabdocsCmd represents the gitlabdocs command
-var gitlabdocsCmd = &cobra.Command{
-	Use:   "gitlabdocs",
+// gitlabCmd represents the gitlabdocs command
+var gitlabCmd = &cobra.Command{
+	Use:   "gitlab",
 	Short: "this command generates docs for gitlab",
 	Long: `This sub command generates your documentation for gitlab:
 			--Currently only terraform is supported
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("gitlabdocs being generated, standby....")
+		fmt.Println("gitlab docs being generated, standby....")
 		err := docbuilder.BuildGitLabDocs(builder)
 		if err != nil {
 			log.Fatal(err)
@@ -33,21 +32,21 @@ var gitlabdocsCmd = &cobra.Command{
 }
 
 func init() {
-	rootCmd.AddCommand(gitlabdocsCmd)
-	gitlabdocsCmd.PersistentFlags().StringSliceVarP(&builder.ProjectIds, "project-ids", "p", []string{}, "comma separated list of project ids for gitlab documentation")
-	err := gitlabdocsCmd.MarkPersistentFlagRequired("project-ids")
+	platformCmd.AddCommand(gitlabCmd)
+	gitlabCmd.PersistentFlags().StringSliceVarP(&builder.ProjectIds, "project-ids", "p", []string{}, "comma separated list of project ids for gitlab documentation")
+	err := gitlabCmd.MarkPersistentFlagRequired("project-ids")
 	if err != nil {
 		log.Printf(err.Error())
 		os.Exit(1)
 	}
-	gitlabdocsCmd.PersistentFlags().StringVarP(&builder.UserName, "user-name", "u", "", "username for authentication with gitlab")
-	err = gitlabdocsCmd.MarkPersistentFlagRequired("user-name")
+	gitlabCmd.PersistentFlags().StringVarP(&builder.UserName, "user-name", "u", "", "username for authentication with gitlab")
+	err = gitlabCmd.MarkPersistentFlagRequired("user-name")
 	if err != nil {
 		log.Printf(err.Error())
 		os.Exit(1)
 	}
-	gitlabdocsCmd.PersistentFlags().StringVarP(&builder.Token, "token", "t", "", "your PAT for authentication with gitlab")
-	err = gitlabdocsCmd.MarkPersistentFlagRequired("token")
+	gitlabCmd.PersistentFlags().StringVarP(&builder.Token, "token", "t", "", "your PAT for authentication with gitlab")
+	err = gitlabCmd.MarkPersistentFlagRequired("token")
 	if err != nil {
 		log.Printf(err.Error())
 		os.Exit(1)
@@ -57,9 +56,9 @@ func init() {
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// gitlabdocsCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// gitlabCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// gitlabdocsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// gitlabCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
